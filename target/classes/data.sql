aee2bbd445cb06ddb908f61b3af6528d09f36e51
luis <lewisv4re@gmail.com>
2025-02-12 19:46:18 -0500

    culminado

---
+++ target/classes/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO cliente (tipo_documento, numero_documento, primer_nombre, segundo_nombre, primer_apellido, segundo_apellido, telefono, direccion, ciudad_residencia, correo_electronico)
+VALUES ('C', '123456789', 'Juan', 'Carlos', 'Perez', 'Gomez', '3001234567', 'Calle 123', 'Bogota', 'juan.perez@example.com');
+
+INSERT INTO cliente (tipo_documento, numero_documento, primer_nombre, segundo_nombre, primer_apellido, segundo_apellido, telefono, direccion, ciudad_residencia, correo_electronico)
+VALUES ('P', '987654321', 'Maria', 'Fernanda', 'Lopez', 'Martinez', '3109876543', 'Carrera 4563', 'Medellin', 'maria.lopez@example.com');
